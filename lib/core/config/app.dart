c7e4cafd0795bd895da05f3f4cb1cf26910b8c9e
Adrian Evangelista <adriane.macer@gmail.com>
2019-12-30 23:39:06 +0800

    Modified web site

--- lib/core/config/app.dart
+++ lib/core/config/app.dart
@@ -1,11 +1,12 @@
 class App {
 //   static String baseUrl = "http://192.168.1.127:80/";
-  static String baseUrl = "http://192.168.100.105:80/";
+//  static String baseUrl = "http://192.168.100.105:80/";
+  static String baseUrl = "https://adriane-macer.github.io/Intelligent_ordering_system/";
   //Note: Virtual Url will not applied. Use Local IP connection
   static String baseUrlWP = baseUrl + "api/v1/"; //with prefix
 
-  static String name = "Tarlac Electric Inc.";
-  static String owner = "";
+  static String name = "Intelligent ordering system";
+//  static String owner = "";
 
   static String playStoreApp =
       "https://play.google.com/store/apps/details?id=org.ns.nsjaafinest&hl=en";
@@ -18,7 +19,7 @@ class App {
   static String privacy = "https://company.meralco.com.ph/privacy-policy";
 
   static const Map<String, String> authorEmail = {
-    'subject': 'TEI MOBILE APP',
-    'address': 'teiph@gmail.com',
+    'subject': 'Intelligent ordering system',
+    'address': '',
   };
 }
